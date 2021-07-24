2ec04e62f5da08c1336c9183462b7273b38114a6
CodeMonkey <ks4510@srmist.edu.in>
2021-07-24 23:04:35 +0530

    pushing changes

---
+++ code_change_1.py
@@ -0,0 +1,81 @@
+#Please change the update status behavior to tweet different messages when download is low and when upload is low and when both are low.
+PROMISED_DOWNLOAD = 20
+PROMISED_UPLOAD = 20
+chrome_driver_path = "C:/Users/LENOVO\Desktop/development/chromedriver.exe"
+
+import time
+import selenium
+from selenium import webdriver
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+
+        
+        
+        time.sleep(3)
+        go_button = self.driver.find_element_by_css_selector(".start-button a")
+        go_button.click()
+
+        time.sleep(60)
+        self.down = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.up = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        print(self.up)
+       
+    def tweet_at_provider(self):
+        import tweepy
+
+# personal details
+        consumer_key ="dfZTldoXKVsBn8zsKHB7Ggokk"
+        consumer_secret ="hbYzNoeNubY7wsABO2vpU5ob0t4IXVVkMNJB0PEyi1QFgoANmK"
+        access_token ="1408727648952487937-pEOOiORcQJgrhh4Oee41CQqedrMafq"
+        access_token_secret ="3k27rNgLA6WrN9jrqhCDtVjaMwnCVoZluGnwUCk2luzAd"
+
+        # authentication of consumer key and secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        # authentication of access token and secret
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        print("Debugging")
+
+    # update the status
+        if float(self.down) < PROMISED_DOWNLOAD or float(self.up) < PROMISED_UPLOAD :
+            tweet1 = f"@RailWireIndia, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWNLOAD}down/{PROMISED_UPLOAD}up?"
+    
+            api.update_status(status = tweet1)
+            print("status updated")
+        elif float(self.down) < PROMISED_DOWNLOAD AND float(self.up) >=PROMISED_UPLOAD:
+            tweet2=f"@RailWireIndia, why is my internet speed {self.down}down when I pay for {PROMISED_DOWNLOAD}down?"
+            api.update_status(status = tweet2)
+            print("status updated")
+        elif float(self.up) < PROMISED_UPLOAD  AND float(self.down) >=PROMISED_DOWNLOAD:
+             tweet3=f"@RailWireIndia, why is my internet speed {self.up}up when I pay for {PROMISED_UPLOAD}up?"
+             api.update_status(status = tweet
+            print("status updated")
+
+    
+
+    
+    
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def some_job():
+    bot = InternetSpeedTwitterBot(chrome_driver_path)
+    bot.get_internet_speed()
+    print(bot.down)
+    bot.tweet_at_provider()
+
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(some_job, 'interval', minutes = 30)
+scheduler.start()
+
---
+++ code_change_3.py
@@ -0,0 +1,71 @@
+PROMISED_DOWNLOAD = 20
+PROMISED_UPLOAD = 20
+chrome_driver_path = "C:/Users/LENOVO\Desktop/development/chromedriver.exe"
+
+import time
+import selenium
+from selenium import webdriver
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self, driver_path):
+        self.driver = webdriver.Chrome(executable_path=driver_path)
+        self.up = 0
+        self.down = 0
+    def get_internet_speed(self):
+        self.driver.get("https://www.speedtest.net/")
+
+        
+        
+        time.sleep(3)
+        go_button = self.driver.find_element_by_css_selector(".start-button a")
+        go_button.click()
+
+        time.sleep(60)
+        self.down = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span').text
+        self.up = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span').text
+        print(self.up)
+       
+    def tweet_at_provider(self):
+        import tweepy
+
+# personal details
+        consumer_key ="dfZTldoXKVsBn8zsKHB7Ggokk"
+        consumer_secret ="hbYzNoeNubY7wsABO2vpU5ob0t4IXVVkMNJB0PEyi1QFgoANmK"
+        access_token ="1408727648952487937-pEOOiORcQJgrhh4Oee41CQqedrMafq"
+        access_token_secret ="3k27rNgLA6WrN9jrqhCDtVjaMwnCVoZluGnwUCk2luzAd"
+
+        # authentication of consumer key and secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        # authentication of access token and secret
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth)
+        print("Debugging")
+
+    # update the status
+        if float(self.down) < PROMISED_DOWNLOAD or float(self.up) < PROMISED_UPLOAD :
+            tweet = f"@RailWireIndia, why is my internet speed {self.down}down/{self.up}up when I pay for {PROMISED_DOWNLOAD}down/{PROMISED_UPLOAD}up?"
+            media = api.media_upload("dog_image.jpg")
+            post_change = api.update_status(status=tweet, media_ids=[media.media_id])
+            
+            print("status with image updated")
+
+    
+    
+
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+def some_job():
+    bot = InternetSpeedTwitterBot(chrome_driver_path)
+    bot.get_internet_speed()
+    print(bot.down)
+    bot.tweet_at_provider()
+
+
+
+scheduler = BlockingScheduler()
+scheduler.add_job(some_job, 'interval', minutes = 1)
+scheduler.start()
+
